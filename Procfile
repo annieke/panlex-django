e89c2151362d59d6ee60d321b68317f4c7c941a6
annie ke <annieke8@gmail.com>
2016-07-20 21:46:27 -0700

    Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PanlexDjango.wsgi --log-file -
---
+++ djangoTD/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/TheMachine/.virtualenvs/djangoTD/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
